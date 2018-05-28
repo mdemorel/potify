55abf27a622bf0f2ce150960bbd9de1d5abaf60a
marc antoine de morel <marcantoine.demorel@gmail.com>
2018-05-28 18:11:45 +0200

    models app potify

--- app/models/first_name.rb
+++
@@ -1,2 +0,0 @@
-class FirstName < ApplicationRecord
-end
--- db/migrate/20180528151040_create_plants.rb
+++ db/migrate/20180528151040_create_plants.rb
@@ -4,14 +4,14 @@ class CreatePlants < ActiveRecord::Migration[5.2]
       t.string :name
       t.string :category
       t.text :description
-      t.date :watering_frequency
+      t.integer :watering_frequency
       t.integer :watering_quantity
-      t.date :potting_frequency
+      t.integer :potting_frequency
       t.integer :fertilizer_quantity
       t.integer :fertilizer_frequency
       t.string :fertilizer_type
       t.integer :exposition
-      t.date :cutting_frequency
+      t.integer :cutting_frequency
       t.integer :temperature
       t.string :photo
 
