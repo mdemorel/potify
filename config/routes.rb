0563104a8912140d8fcbd12e4c5532368dc7219a
marc antoine de morel <marcantoine.demorel@gmail.com>
2018-05-29 12:33:05 +0200

    dashboard controller, page and routes created

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  get 'dashboard', to: 'pages#dashboard', as: :dashboard
+
   resources :plants, only: [:index, :show]
   resources :pots, only: [:index, :show, :new, :create, :edit, :update, :destroy]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
