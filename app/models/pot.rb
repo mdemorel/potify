8d07c7aadd162d545d50153a0ddb154341c74cba
marc antoine de morel <marcantoine.demorel@gmail.com>
2018-05-30 16:48:35 +0200

    cloudinary

--- app/models/pot.rb
+++ app/models/pot.rb
@@ -2,4 +2,6 @@ class Pot < ApplicationRecord
   belongs_to :user
   belongs_to :plant
   has_many :tasks, dependent: :destroy
+
+  mount_uploader :photo, PhotoUploader
 end
--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,3 +1,4 @@
 class PhotoUploader < CarrierWave::Uploader::Base
  include Cloudinary::CarrierWave
+
 end
