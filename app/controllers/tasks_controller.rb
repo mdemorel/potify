7e45316c76e83ad81ce2f0f987c015e964cf5ef4
Elsa Szymczak <elsaszymczak@gmail.com>
2018-05-30 15:01:43 +0200

    created the first step of tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,3 +1,24 @@
 class TasksController < ApplicationController
+  def new
+    @task = Task.new
+  end
 
+  def create
+    @task = Task.new(task_params)
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :completed, :points, :pot_id)
+  end
 end
