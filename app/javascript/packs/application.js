957a9e1c8c0bfc1a6bc1fd7f9c624b8353bf01fe
marc antoine de morel <marcantoine.demorel@gmail.com>
2018-05-31 13:57:40 +0200

    toggle cards working, submit btn -> btn primaray

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,11 @@
 import "bootstrap";
 import "../plugins/flatpickr";
 import "../components/select_size";
+import { sizeChoice } from "../components/size_choice";
 
 
 // importer la fonction selectSize
 // l'appeler
 // import {selectSize} from "../components/select_size";
-// selectSize();
+sizeChoice();
 
