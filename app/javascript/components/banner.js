1c2710e4604c3af0d206401eb967b911026729bb
Elsa Szymczak <elsaszymczak@gmail.com>
2018-05-31 11:42:35 +0200

    some changes

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -1,13 +1,14 @@
 import swal from 'sweetalert';
 
-function bindSweetAlertButtonDemo() {
+function bindSweetAlertButtonDemo(pot_id) {
   const swalButton = document.querySelectorAll('.sweet-alert-demo');
   if (swalButton) {
     swalButton.forEach(function(button) {
       button.addEventListener('click', () => {
         swal({
           title: "Youhou, you did great!",
-          icon: "success"
+          icon: "success",
+          timer: 30000,
         });
       });
     }) // protect other pages
