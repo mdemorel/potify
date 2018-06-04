34479cff456f82ec96da9687b2e18613e16a3302
Louise Darche <louise.darche@gmail.com>
2018-06-04 15:16:40 +0200

    can update and delete only when your plant - design needed

--- app/policies/pot_policy.rb
+++ app/policies/pot_policy.rb
@@ -9,6 +9,14 @@ class PotPolicy < ApplicationPolicy
     return true
   end
 
+  def edit?
+    update?
+  end
+
+  def update?
+    record.user == user
+  end
+
   def destroy?
     record.user == user
   end
