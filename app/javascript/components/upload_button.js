97fde814b108c5dd4d14865ad6bf9fe160ecde1b
marc antoine de morel <marcantoine.demorel@gmail.com>
2018-06-07 10:45:33 +0200

    uplaod button smarter

--- app/javascript/components/upload_button.js
+++ app/javascript/components/upload_button.js
@@ -2,9 +2,7 @@ function fireUploadButton () {
   const fakeButton = document.getElementById('upload-fake-btn');
   fakeButton.addEventListener("click", (e) => {
     e.preventDefault();
-    console.log(e);
 
-  const event = new Event('click');
   const targetedButton = document.getElementById('pot_photo');
   targetedButton.click();
   });
